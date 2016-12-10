911a8cbde60f71c090e07aafbdd8288edd51135d
Gregor Richards <hg-yff@gregor.im>
2016-12-09 19:04:00 -0500

    Report git version with library_version

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -252,7 +252,10 @@ void retro_get_system_info(struct retro_system_info *info)
 {
    memset(info, 0, sizeof(*info));
    info->library_name     = "Hatari";
-   info->library_version  = "1.8";
+#ifndef GIT_VERSION
+#define GIT_VERSION ""
+#endif
+   info->library_version  = "1.8" GIT_VERSION;
    info->valid_extensions = "ST|MSA|ZIP|STX|DIM|IPF";
    info->need_fullpath    = true;
    info->block_extract = false;
