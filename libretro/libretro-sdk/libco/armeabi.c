81981eeb30ac051d7be056d5d48c4bda56f69330
Aaron Kling <webgeek1234@gmail.com>
2019-05-24 21:24:07 -0500

    Update libco
    Fixes clang compile issues

--- libretro/libretro-sdk/libco/armeabi.c
+++ libretro/libretro-sdk/libco/armeabi.c
@@ -11,7 +11,7 @@
 #include <string.h>
 #include <stdint.h>
 
-#ifndef IOS
+#ifndef __APPLE__
 #include <malloc.h>
 #endif
 
@@ -22,19 +22,19 @@ extern "C" {
 static thread_local uint32_t co_active_buffer[64];
 static thread_local cothread_t co_active_handle;
 
-__asm__ (
-      "\n"
-      ".text\n"
+asm (
+      ".arm\n"
+      ".align 4\n"
       ".globl co_switch_arm\n"
-      ".type co_switch_arm,STT_FUNC\n"
-      ".p2align 4\n"
+      ".globl _co_switch_arm\n"
       "co_switch_arm:\n"
+      "_co_switch_arm:\n"      
       "  stmia r1!, {r4, r5, r6, r7, r8, r9, r10, r11, sp, lr}\n"
       "  ldmia r0!, {r4, r5, r6, r7, r8, r9, r10, r11, sp, pc}\n"
     );
 
 /* ASM */
-extern void co_switch_arm(cothread_t handle, cothread_t current);
+void co_switch_arm(cothread_t handle, cothread_t current);
 
 static void crash(void)
 {
@@ -46,7 +46,7 @@ cothread_t co_create(unsigned int size, void (*entrypoint)(void))
 {
    size = (size + 1023) & ~1023;
    cothread_t handle = 0;
-#if HAVE_POSIX_MEMALIGN >= 1
+#if defined(__APPLE__) || HAVE_POSIX_MEMALIGN >= 1
    if (posix_memalign(&handle, 1024, size + 256) < 0)
       return 0;
 #else
@@ -66,7 +66,8 @@ cothread_t co_create(unsigned int size, void (*entrypoint)(void))
    ptr[5] = 0; /* r9  */
    ptr[6] = 0; /* r10 */
    ptr[7] = 0; /* r11 */
-   ptr[8] = (uintptr_t)ptr + size + 256; /* r13, stack pointer */
+   /* Align stack to 64-bit */
+   ptr[8] = (uintptr_t)ptr + size + 256 - 8; /* r13, stack pointer */
    ptr[9] = (uintptr_t)entrypoint; /* r15, PC (link register r14 gets saved here). */
    return handle;
 }
