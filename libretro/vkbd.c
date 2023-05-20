d24ff1530535d6c8215f98cee62085e2a1364a7d
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-05-21 00:45:56 +0300

    improve virtual keyboard
    change keys position and colors closer to an ST

--- libretro/vkbd.c
+++ libretro/vkbd.c
@@ -15,21 +15,21 @@ void virtual_kdb(unsigned short int *pixels,int vx,int vy)
 
    page = (NPAGE == -1) ? 0 : 50;
    pix= (short unsigned *)&pixels[0];
-   coul = RGB565(28, 28, 31);
-   BKGCOLOR = (KCOL>0?0x8080:0);
+   coul = RGB565(1, 1, 1);   // text color
+   BKGCOLOR = (KCOL>0?0x9cb3:0);  //text background color
 
-   for(x=0;x<NPLGN;x++)
+   for(x=0;x<NPLGN;x++)  // 10 letters horiz.
    {
-      for(y=0;y<NLIGN;y++)
+      for(y=0;y<NLIGN;y++)  // 5 letters vert.
       {
-         DrawBoxBmp(pix,XBASE3+x*XSIDE,YBASE3+y*YSIDE, XSIDE,YSIDE, RGB565(7, 2, 1));
+         DrawBoxBmp(pix,XBASE3+x*XSIDE,YBASE3+y*YSIDE, XSIDE,YSIDE, RGB565(3, 7, 3)); // unselected
          Draw_text(pix,XBASE0-2+x*XSIDE ,YBASE0+YSIDE*y,coul, BKGCOLOR ,2, 2,20,
                SHIFTON==-1?MVk[(y*NPLGN)+x+page].norml:MVk[(y*NPLGN)+x+page].shift);	
       }
    }
 
-   DrawBoxBmp(pix,XBASE3+vx*XSIDE,YBASE3+vy*YSIDE, XSIDE,YSIDE, RGB565(31, 2, 1));
-   Draw_text(pix,XBASE0-2+vx*XSIDE ,YBASE0+YSIDE*vy,RGB565(2,31,1), BKGCOLOR ,2, 2,20,
+   DrawBoxBmp(pix,XBASE3+vx*XSIDE,YBASE3+vy*YSIDE, XSIDE,YSIDE, RGB565(31, 0, 31)); //selected
+   Draw_text(pix,XBASE0-2+vx*XSIDE ,YBASE0+YSIDE*vy,RGB565(30,57,0), BKGCOLOR ,2, 2,20,
          SHIFTON==-1?MVk[(vy*NPLGN)+vx+page].norml:MVk[(vy*NPLGN)+vx+page].shift);	
 }
 
