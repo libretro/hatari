24e088bdbe2b829f3616dbd2581cec8e302a0d1d
metallic77 <43163462+metallic77@users.noreply.github.com>
2023-05-23 23:01:47 +0300

    add Atari SC1224 visible resolution as "cropped"
    366x243 is the actual visible resolution on an Atari SC1224 monitor.

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -343,8 +343,8 @@ static void update_variables(void)
             hatari_borders = true;
             break;
          case HATARI_VIDEO_CR_LO:
-            retrow = 320;
-            retroh = 200;
+            retrow = 366;
+            retroh = 243;
             // Strange, do not work if set to false...
             hatari_borders = true;
             break;
@@ -354,8 +354,8 @@ static void update_variables(void)
             hatari_borders = true;
             break;
          case HATARI_VIDEO_CR_HI:
-            retrow = 832;
-            retroh = 548;
+            retrow = 732;
+            retroh = 486;
             hatari_borders = false;
             break;
       }
