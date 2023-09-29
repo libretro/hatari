d977079f7a7b2963e72adc13a5b10f70ebb8162a
Greenchili2 <shannonpope2000@gmail.com>
2023-09-29 03:36:06 -0400

    Attempt 3 to appease the Wii buildbot gods.  Reactivated Nintendo Wii on buildbot.

--- src/rs232.c
+++ src/rs232.c
@@ -21,7 +21,7 @@ const char RS232_fileid[] = "Hatari rs232.c : " __DATE__ " " __TIME__;
 # include <termios.h>
 # include <unistd.h>
 #endif
-#if defined(WIIU) || defined(VITA)
+#if defined(WIIU) || defined(VITA) || defined(GEKKO)
 //no rs323 for now
 #else
 #include <SDL.h>
@@ -272,7 +272,7 @@ static void RS232_CloseCOMPort(void)
 	Dprintf(("Closed RS232 files.\n"));
 }
 
-#if !defined(WIIU) && !defined(VITA)
+#if !defined(WIIU) && !defined(VITA) && !defined(GEKKO)
 /* thread stuff */
 static SDL_sem* pSemFreeBuf;       /* Semaphore to sync free space in InputBuffer_RS232 */
 static SDL_Thread *RS232Thread = NULL; /* Thread handle for reading incoming data */
@@ -285,7 +285,7 @@ static SDL_Thread *RS232Thread = NULL; /* Thread handle for reading incoming dat
 static void RS232_AddBytesToInputBuffer(unsigned char *pBytes, int nBytes)
 {
 	int i;
-#if !defined(WIIU) && !defined(VITA)
+#if !defined(WIIU) && !defined(VITA) && !defined(GEKKO)
 	/* Copy bytes into input buffer */
 	for (i=0; i<nBytes; i++)
 	{
@@ -351,7 +351,7 @@ static int RS232_ThreadFunc(void *pData)
  */
 void RS232_Init(void)
 {
-#if !defined(WIIU) && !defined(VITA)
+#if !defined(WIIU) && !defined(VITA) && !defined(GEKKO)
 	if (ConfigureParams.RS232.bEnableRS232)
 	{
 		if (!RS232_OpenCOMPort())
@@ -399,7 +399,7 @@ void RS232_Init(void)
  */
 void RS232_UnInit(void)
 {
-#if !defined(WIIU) && !defined(VITA)
+#if !defined(WIIU) && !defined(VITA) && !defined(GEKKO)
 	/* Close, kill thread and free resource */
 	if (RS232Thread)
 	{
@@ -664,7 +664,7 @@ bool RS232_TransferBytesTo(Uint8 *pBytes, int nBytes)
 bool RS232_ReadBytes(Uint8 *pBytes, int nBytes)
 {
 	int i;
-#if !defined(WIIU) && !defined(VITA)
+#if !defined(WIIU) && !defined(VITA) && !defined(GEKKO)
 	/* Connected? */
 	if (hComIn && InputBuffer_Head != InputBuffer_Tail)
 	{
