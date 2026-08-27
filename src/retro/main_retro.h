b5b1997bf1650f7896c036785641e1bc478a4f08
Craig Carnell <1188869+cscd98@users.noreply.github.com>
2026-08-27 22:24:46 +0100

    Add tos.img lookup in libretro sys directory

--- src/retro/main_retro.h
+++ src/retro/main_retro.h
@@ -5,6 +5,14 @@
   or at your option any later version. Read the file gpl.txt for details.
 */
 
+#include "libretro.h"
+
+#ifdef _WIN32
+#define RETRO_PATH_SEPARATOR   		"\\"
+#else
+#define RETRO_PATH_SEPARATOR   		"/"
+#endif
+
 extern retro_environment_t environment_cb;
 extern retro_video_refresh_t video_refresh_cb;
 extern retro_input_state_t input_state_cb;
